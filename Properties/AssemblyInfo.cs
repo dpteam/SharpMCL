2c3e31311e6f04d514df1c8e0a46bc7ec5dbcfb0
DartPower <SIMPSON-NEKEK@mail.ru>
2019-06-03 00:01:58 +0300

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
 // для компонентов COM. Если необходимо обратиться к типу в этой сборке через
 // из модели COM задайте для атрибута ComVisible этого типа значение true.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
 [assembly: Guid("4ccd1f91-1fe9-4611-9486-95bef07901b0")]
